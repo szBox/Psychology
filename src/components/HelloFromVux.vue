53b0014eb6405a88f35fc9039dea8fb6cac9356d
粟振 <1091271822@qq.com>
2018-04-09 14:20:44 +0800

    心理预约—_初始

---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h1 @click="c()">1 </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="VUX" value="cool" is-link></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell  } from 'vux'
+//Vue.use(ToastPlugin)
+export default {
+  components: {
+    Group,
+    Cell,
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  },
+  methods:{
+  	c(){
+  		this.$vux.toast.show({
+				 type:'text',
+				 text: 'yes',
+				 position:'bottom'
+				})
+  	}
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
